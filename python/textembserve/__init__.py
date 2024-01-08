7d47a3f0f6e49b7265346d0705bc5e12b464e310
hieupth <64821726+hieupth@users.noreply.github.com>
2024-01-08 19:22:11 +0700

    chore: link rust tokenizer to library

--- python/textembserve/__init__.py
+++ python/textembserve/__init__.py
@@ -1,6 +1,6 @@
 from .textembserve import *
 
-
+# For convenience import
 __doc__ = textembserve.__doc__
 if hasattr(textembserve, "__all__"):
     __all__ = textembserve.__all__
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,10 @@
 use pyo3::prelude::*;
+mod tokenizer;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
 
-/// A Python module implemented in Rust.
 #[pymodule]
-fn textembserve(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+#[pyo3(name = "textembserve")]
+fn my_module(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_class::<tokenizer::RustTokenizer>()?;
     Ok(())
 }
