a76968f84e73554df077912b12910824f198988c
hieupth <64821726+hieupth@users.noreply.github.com>
2024-01-09 13:10:43 +0700

    fix: vncorenlp path

--- python/textembserve/wordsegmenters/vncorenlp.py
+++ python/textembserve/wordsegmenters/vncorenlp.py
@@ -3,9 +3,6 @@ import py_vncorenlp
 from . import WordSegmenter
 
 
-SAVE_DIR = os.getenv("VNCORENLP_DIR", os.path.expanduser("~/.vncorenlp"))
-
-
 class VnCoreNLPWordSegmenter(WordSegmenter):
   """
   This class perform Vietnamese word segmentation via VnCoreNLP lib.
@@ -17,9 +14,11 @@ class VnCoreNLPWordSegmenter(WordSegmenter):
     """
     # Automatically download VnCoreNLP components from the original repository
     # and save them in some local working folder.
-    py_vncorenlp.download_model(save_dir=SAVE_DIR)
+    dir = os.getenv("VNCORENLP_DIR", os.path.expanduser("~/.vncorenlp"))
+    os.makedirs(dir, exist_ok=True)
+    py_vncorenlp.download_model(save_dir=dir)
     # Load the word and sentence segmentation component.
-    self._segmenter = py_vncorenlp.VnCoreNLP(annotators=["wseg"], save_dir=SAVE_DIR)
+    self._segmenter = py_vncorenlp.VnCoreNLP(annotators=["wseg"], save_dir=dir)
   
   def _segment(self, message: str) -> str:
     """
