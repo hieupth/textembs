9d6b458213ce4b9e618f7c310e8a0983c749601c
hieupth <64821726+hieupth@users.noreply.github.com>
2024-01-09 13:11:38 +0700

    chore: add create word segmenter func

--- python/textembserve/wordsegmenters/__init__.py
+++ python/textembserve/wordsegmenters/__init__.py
@@ -1,2 +1,13 @@
 from .base import WordSegmenter
 from .vncorenlp import VnCoreNLPWordSegmenter
+
+
+def create_word_segmenter(name: str):
+  if name is None:
+    return WordSegmenter()
+  else:
+    name = name.lower()
+    if name == "vncorenlp":
+      return VnCoreNLPWordSegmenter()
+    else:
+      return WordSegmenter()
